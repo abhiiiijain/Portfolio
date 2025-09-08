a90b2b4a1289faeb7e04c9218137a3ca7a410f01
abhiiiijain <abhinandanbansal123@gmail.com>
2025-09-08 10:46:17 +0530

    footer updated

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Credits() {
   return (
-    <div className="credits"><a>Designed by Abhinandan Jain</a></div>
+    <div className="credits"><a>Designed by abhiiiijain</a></div>
   )
 }
 
